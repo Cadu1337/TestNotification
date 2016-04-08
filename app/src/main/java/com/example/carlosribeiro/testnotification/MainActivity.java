48d402b7fcab5ea7b5d5f332e434b71476b59585
Carlos Eduardo <kadu-kun@hotmail.com>
2016-04-08 12:32:54 -0300

    Initial Commit
    Test project to learn how to use notifications on Android.

---
+++ app/src/main/java/com/example/carlosribeiro/testnotification/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.carlosribeiro.testnotification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    Intent intent = new Intent();
+                    PendingIntent pIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
+                    Notification noti =  new Notification.Builder(MainActivity.this)
+                            .setTicker("TickerTitle")
+                            .setContentTitle("Content Text cadu")
+                            .setSmallIcon(R.drawable.ic_launcher)
+                            .addAction(R.drawable.ic_launcher, "Action 1", pIntent)
+                            .addAction(R.drawable.ic_launcher, "Action 2", pIntent)
+                            .setContentIntent(pIntent).getNotification();
+
+                    noti.flags = Notification.FLAG_AUTO_CANCEL;
+                    NotificationManager nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                    nm.notify(0,noti);
+                }
+            }
+        );
+    }
+}
+
